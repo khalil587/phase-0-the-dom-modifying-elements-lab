249967b55596878bf827523bc5c182e289b1dc33
Khalil Lindsay <lindsay.khalil@gmail.com>
2021-05-10 19:48:00 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const e = document.querySelector("main");
+e.parentElement.remove(e);
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "is the champion";
+
